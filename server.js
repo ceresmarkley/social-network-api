e5e2357f5258c35c96b97f07eb5adb4ba63c102b
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-09-05 19:54:21 -0400

    created connection and server code. established socialNetworkDB title, and server connections for furture routes.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`🌎 Network server is running on http://localhost:${PORT} 🚀!`);
+    });
+});
