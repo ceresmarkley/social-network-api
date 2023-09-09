10cff2c774da764666f684b27716d8974f163969
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-09-09 14:46:03 -0400

    added create and update User function to user controller js.

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -26,10 +26,40 @@ module.exports = {
             if (!userData) {
                 return res.status(404).json({ message: 'Oops! Could not recognize or find this user!'})
             }
-            res.json(thoughtData);
+            res.json(userData);
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
+        }
+    },
+
+    // CREATE a new user
+    async createUser(req, res) {
+        try {
+            const userData = await User.create(req.body);
+            res.json(userData);
+        } catch (err) {
+            console.log(err);
+            res.status(500),json(err);
+        }
+    },
+
+    // UPDATE a user by ID
+    async updateUser(req, res) {
+        try {
+            const userData = await User.findOneAndUpdate({ _id: req.params.userID},
+                { $set: req.body },
+                // verify that the updated infomation matches User model format and return the new user instead of the old(original).
+                { runValidators: true, new:true });
+
+                if (!userData) {
+                    return res.status(404).json({ message: 'Oops! Could not recognize or find this user!'})
+                }
+
+                res.json(userData);
+        } catch (err) {
+            console.log(err);
+            res.status(500),json(err);
         }
     },
 }
